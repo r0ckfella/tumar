ab0681d431ea52c5d494dbf611d75d222103db8a
r0ckfella <epmek96@gmail.com>
2019-12-11 17:47:01 +0600

    Added phone number field to User

--- tumar/users/models.py
+++ tumar/users/models.py
@@ -13,8 +13,8 @@ from rest_framework.authtoken.models import Token
 @python_2_unicode_compatible
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
-                                 message=("Phone number must be entered in the format: '+999999999'."
+    phone_regex = RegexValidator(regex=r'^\+?\d?\d{9,15}$',
+                                 message=("Phone number must be entered in the format: '+999999999'. "
                                           "Up to 15 digits allowed."))
     phone_num = models.CharField(validators=[phone_regex], max_length=17, blank=True)
 
