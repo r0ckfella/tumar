5d5f4f951158369df482a305be8590de77fe4178
r0ckfella <epmek96@gmail.com>
2019-12-13 03:07:55 +0600

    Now factories generate points only near the center of KZ; factories have an option not to generate parents

--- tumar/animals/test/factories.py
+++ tumar/animals/test/factories.py
@@ -1,4 +1,6 @@
 import datetime
+from decimal import Decimal
+
 import factory.fuzzy
 import random
 
@@ -6,12 +8,15 @@ from django.contrib.gis.geos import Point
 from faker import Factory as FakerFactory
 from pytz import timezone
 
+from ..models import Farm, Animal, Geolocation
+
 faker = FakerFactory.create()
 
 
 class FarmFactory(factory.django.DjangoModelFactory):
     class Meta:
-        model = 'animals.Farm'
+        model = Farm
+        django_get_or_create = ('name', 'key')
 
     id = factory.Faker('uuid4')
     key = factory.Faker('hexify', text="^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^", upper=False)
@@ -20,10 +25,11 @@ class FarmFactory(factory.django.DjangoModelFactory):
 
 class AnimalFactory(factory.django.DjangoModelFactory):
     class Meta:
-        model = 'animals.Animal'
+        model = Animal
+        django_get_or_create = ('cow_code', 'imei',)
 
     id = factory.Faker('uuid4')
-    farm = factory.SubFactory(FarmFactory)
+    farm = factory.Iterator(Farm.objects.all())
     imei = factory.Faker('numerify', text="###############")
 
     @factory.lazy_attribute
@@ -31,13 +37,17 @@ class AnimalFactory(factory.django.DjangoModelFactory):
         species = ['Cow', 'Horse', 'Sheep']
         return random.choice(species) + " " + faker.first_name()
 
+    class Params:
+        parents = factory.Trait(
+            farm=factory.SubFactory(FarmFactory),
+        )
+
 
 class GeolocationFactory(factory.django.DjangoModelFactory):
     class Meta:
-        model = 'animals.Geolocation'
+        model = Geolocation
 
-    animal = factory.SubFactory(AnimalFactory)
-    # time = factory.fuzzy.FuzzyDateTime(datetime.datetime(2016, 1, 1, tzinfo=timezone('Asia/Almaty')))
+    animal = factory.Iterator(Animal.objects.all())
     time = factory.Faker('date_time_between',
                          tzinfo=timezone('Asia/Almaty'),
                          end_date="now",
@@ -46,5 +56,11 @@ class GeolocationFactory(factory.django.DjangoModelFactory):
 
     @factory.lazy_attribute
     def position(self):
-        lat, lon = faker.local_latlng(country_code="KZ", coords_only=True)
+        lat = faker.coordinate(center=Decimal('52.005850'), radius=0.2)
+        lon = faker.coordinate(center=Decimal('67.821587'), radius=0.4)
         return Point(float(lon), float(lat), srid=4326)
+
+    class Params:
+        parents = factory.Trait(
+            animal=factory.SubFactory(AnimalFactory),
+        )
