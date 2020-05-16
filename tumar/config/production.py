82f9019ec2761465b82a824e22b264665b130396
r0ckfella <epmek96@gmail.com>
2020-05-16 08:15:57 +0600

    added logging to telegram bot in prod environment

--- tumar/config/production.py
+++ tumar/config/production.py
@@ -8,32 +8,8 @@ from .common import Common
 class Production(Common):
     INSTALLED_APPS = Common.INSTALLED_APPS
     SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
-    # Site
-    # https://docs.djangoproject.com/en/2.0/ref/settings/#allowed-hosts
-    ALLOWED_HOSTS = ["*"]
-    # INSTALLED_APPS += ("gunicorn", )
-
-    # Static files (CSS, JavaScript, Images)
-    # https://docs.djangoproject.com/en/2.0/howto/static-files/
-    # http://django-storages.readthedocs.org/en/latest/index.html
-    # INSTALLED_APPS += ('storages',)
-    # DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    # AWS_ACCESS_KEY_ID = os.getenv('DJANGO_AWS_ACCESS_KEY_ID')
-    # AWS_SECRET_ACCESS_KEY = os.getenv('DJANGO_AWS_SECRET_ACCESS_KEY')
-    # AWS_STORAGE_BUCKET_NAME = os.getenv('DJANGO_AWS_STORAGE_BUCKET_NAME')
-    # AWS_DEFAULT_ACL = 'public-read'
-    # AWS_AUTO_CREATE_BUCKET = True
-    # AWS_QUERYSTRING_AUTH = False
-    # MEDIA_URL = f'https://s3.amazonaws.com/{AWS_STORAGE_BUCKET_NAME}/'
-
-    # https://developers.google.com/web/fundamentals/performance/optimizing-content-efficiency/http-caching#cache-control
-    # Response can be cached by browser and any intermediary caches
-    #   (i.e. it is "public") for up to 1 day
-    # 86400 = (60 seconds x 60 minutes x 24 hours)
-    # AWS_HEADERS = {
-    #     'Cache-Control': 'max-age=86400, s-maxage=86400, must-revalidate',
-    # }
+    ALLOWED_HOSTS = ["arys.egistic.kz"]
+    INSTALLED_APPS += ("django_log_to_telegram",)
     STATIC_ROOT = "/static/"
 
     # Postgis
@@ -69,3 +45,28 @@ class Production(Common):
     CELERY_CREATE_MISSING_QUEUES = True
     CELERY_TASK_REMOTE_TRACEBACKS = True
     CELERY_TASK_DEFAULT_QUEUE = "tumar_queue"
+
+    LOG_TO_TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+
+    LOGGING = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "filters": {
+            "require_debug_false": {"()": "django.utils.log.RequireDebugFalse"}
+        },
+        "handlers": {
+            "telegram_log": {
+                "level": "ERROR",
+                "filters": ["require_debug_false"],
+                "class": "django_log_to_telegram.log.AdminTelegramHandler",
+                "bot_token": LOG_TO_TELEGRAM_BOT_TOKEN,
+            }
+        },
+        "loggers": {
+            "django.request": {
+                "handlers": ["telegram_log"],
+                "level": "ERROR",
+                "propagate": True,
+            },
+        },
+    }
