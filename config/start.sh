453f4703323c7ffa963836cef0a4806bad7c3b09
r0ckfella <epmek96@gmail.com>
2020-06-30 01:38:51 +0600

    fixing typos

--- config/start.sh
+++ config/start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # start django in production env
-python ../manage.py collectstatic --noinput &&
-python ../manage.py migrate &&
-python ../manage.py compilemessages &&
+python ./manage.py collectstatic --noinput &&
+python ./manage.py migrate &&
+python ./manage.py compilemessages &&
 uwsgi --ini ./uwsgi.ini
 
 # if [ ${PRODUCTION} == "false" ]; then
