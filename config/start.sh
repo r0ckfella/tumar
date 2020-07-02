e0f4b8a8a344d2eddd37b3083f522b1308820588
r0ckfella <epmek96@gmail.com>
2020-07-02 19:11:38 +0600

    fixing root permissions

--- config/start.sh
+++ config/start.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # start django in production env
+export CURRENT_UID=$(id -u) CURRENT_GID=$(id -g)
 python ./manage.py collectstatic --noinput &&
 python ./manage.py migrate &&
 python ./manage.py compilemessages &&
-uwsgi --ini ./config/uwsgi.ini
+uwsgi --ini ./config/uwsgi.ini --uid ${CURRENT_ID} --gid ${CURRENT_GID}
 
 # if [ ${PRODUCTION} == "false" ]; then
 #     # use development server
